0cd34a2a617ae5f0fd421a5bfbcc6b5427efa0e9
Farnaz <farnazfawadhasan@gmail.com>
2023-04-30 23:55:53 +0600

    added

--- data_models/playground.py
+++ data_models/playground.py
@@ -1,4 +1,4 @@
 from data_models.pokemon import Pokemon
 
 squirtle = Pokemon('squirtle', '004', 10, 15, 8, 20, [('tackle', 5)])
-squirtle2 = Pokemon('squirtle2', '004', 10, 15, 8, 20, [('tackle', 5)])
+squirtle2454454 = Pokemon('squirtle2', '004', 10, 15, 8, 20, [('tackle', 5)])
